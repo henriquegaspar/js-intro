d57aa4e23fef726fb9cd0a97115f0f892d77fb35
henriquegaspar <henriquegaspar@live.com>
2016-04-11 18:57:58 -0500

    fdngfjg

--- 02_files/scripts.js
+++ 02_files/scripts.js
@@ -1,4 +1,4 @@
 alert("Help me fix this program!");
 alert("Can you get this message to appear?");
-document.write("<h2>My first JavaScript program</h2>");
+document.write("<h2>Messi is the best player ever</h2>");
 document.write("I'm practicing 'debugging'.");
---
+++ 04_vars/scripts.js
@@ -0,0 +1,13 @@
+var heading = "";
+var content = "";
+
+heading +="this heading";
+
+content+="<h1>" + heading + "</h1>";
+
+content+="<p>the first text area.</p>"
+
+document.write(content);
+
+//console.log(content);
+
